f4f9b92e5e611545b15f99bd8ddb8db1740a0884
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-09 19:51:26 -0700

    Properly apply flex to header

--- app/layout.js
+++ app/layout.js
@@ -12,26 +12,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
-      <body className={
-        'min-h-screen flex flex-col relative ' + inter.className
-      }>
-        <header id='header'
-          className='sticky top-0 p-6 bg-white border-b border-solid border-blue-900
-        shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8
-        flex item-center justify-between'
-        >
+
+      <body className={'min-h-screen flex flex-col relative ' + inter.className}>
+
+        <header className='flex items-center justify-between sticky
+        top-0 p-6 bg-slate-200 border-b border-solid border-blue-900
+        shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8'>
           <Link href={'/'}>
             <h1 className='uppercase cursor-pointer hover:scale-110'>Shop</h1>
-            <i className="fa-solid fa-cart-shopping"></i>
           </Link>
+            <i className="fa-solid fa-cart-shopping cursor-pointer
+              hover:text-slate-500"></i>
         </header>
+
         <div className="flex-1">
           {children}
         </div>
+
         <footer className=''>FOOTER</footer>
+
       </body>
     </html>
   )
