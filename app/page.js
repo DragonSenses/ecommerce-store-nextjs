1337a7bf093ae15c0fdff0aa2f397953185180f7
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-09 20:27:22 -0700

    Initialize Stripe

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 import Image from 'next/image'
+import Stripe from 'stripe'
 
-export default function Home() {
+async function getStripeProducts(){
+  const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+    apiVersion: '2020-08-27'
+  });
+  const res = await stripe.prices.list({
+
+  });
+}
+
+export default async function Home() {
   return (
-    <main className="">
-      
+    <main className="p-4">
+      Main Text
     </main>
   )
 }
