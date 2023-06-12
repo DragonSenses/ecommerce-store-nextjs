bdd3638a1e62678315a2839b4576f7662e994e8d
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-11 18:12:37 -0700

    Map out products to ProductCard

--- app/page.js
+++ app/page.js
@@ -25,9 +25,13 @@ export default async function Home() {
 
   return (
     <main className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-      {}
-      <ProductCard />
+      {products.map((product, productIndex) => {
 
+        return (
+          <ProductCard key={productIndex} product={product}/>
+        )
+
+      })}
     </main>
   )
 }
