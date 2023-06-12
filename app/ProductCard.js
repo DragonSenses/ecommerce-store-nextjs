015645be0c962819007721e73e014821cdffe731
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-11 18:28:39 -0700

    Extract data from props

--- app/ProductCard.js
+++ app/ProductCard.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default function ProductCard(props) {
+  const { product } = props;
+  
+  const {
+    id: price_id,
+    unit_amount: cost,
+    product: productInfo,
+  } = product;
+
   return (
     <div>ProductCard</div>
   )
