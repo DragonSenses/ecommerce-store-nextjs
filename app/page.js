fc9b2c47b0f65ac76a997fc3f5f1920a3b1f2b98
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-11 18:40:33 -0700

    Add gap between ProductCards

--- app/page.js
+++ app/page.js
@@ -25,8 +25,8 @@ export default async function Home() {
 
   return (
     <main className="p-4 flex flex-col">
-      <div className="max-w-[1000px] w-full mx-auto grid grid-cols-1 
-      sm:grid-cols-2 md:grid-cols-3">
+      <div className="max-w-[1000px] w-full mx-auto grid grid-cols-1
+      sm:grid-cols-2 md:grid-cols-3 gap-4">
         {products.map((product, productIndex) => {
 
           return (
