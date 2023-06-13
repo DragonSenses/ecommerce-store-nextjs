5d4e5b578030ad7dec16891d798f01c9755c05d4
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-12 20:15:05 -0700

    Update ProductCard.js

--- app/ProductCard.js
+++ app/ProductCard.js
@@ -16,11 +16,18 @@ export default function ProductCard(props) {
 
   return (
     <div className='flex flex-col shadow bg-white hover:shadow-lg
-    cursir-point'>
+    cursor-pointer'>
       <img 
         src={productInfo.images[0]} alt={name} 
         className='w-full h-full object-cover' 
       />
+      <div className="flex flex-col gap-2 p-4">
+        <div className="flex items-center justify-between">
+          <h3>{name}</h3>
+          <p>${cost/100}</p>
+        </div>
+        <p className='text-sm'>{description}</p>
+      </div>
     </div>
   )
 }
