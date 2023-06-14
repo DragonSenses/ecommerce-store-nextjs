e45f923d4fca1e8b9a83fc26639e71467edf1e63
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-13 20:39:00 -0700

    Extract data from props

--- app/product/page.js
+++ app/product/page.js
@@ -1,4 +1,11 @@
-export default function ProductPage() {
+export default function ProductPage(props) {
+  const { searchParams } = props;
+
+  const { price_id } = props;
+
+  console.log(searchParams);
+  console.log(price_id);
+
   return(
     <div>
       Hello, this is the Product Page.
