aeda1b42374b2cf537120560c4347350cf8a508e
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-15 15:19:14 -0700

    Bind component with product

--- app/product/page.js
+++ app/product/page.js
@@ -1,10 +1,16 @@
+import useCart from "../(store)/store";
+
 export default function ProductPage(props) {
   const { searchParams } = props;
 
   const { price_id } = props;
 
+  const product = useCart(state => state.product);
+
+  // log the variables
   console.log(searchParams);
   console.log(price_id);
+  console.log(product);
 
   return(
     <div>
