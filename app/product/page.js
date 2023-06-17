7919d903cad2d4452029954344e8c67455902a4a
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-16 21:05:14 -0700

    Extract price_id from query

--- app/product/page.js
+++ app/product/page.js
@@ -5,19 +5,22 @@ import useCart from "../(store)/store";
 export default function ProductPage(props) {
   const { searchParams } = props;
 
-  const { price_id } = props;
+  const { price_id } = searchParams;
 
   const product = useCart(state => state.product);
 
   // log the variables
+  console.log('searchParams are:')
   console.log(searchParams);
+  console.log('price_id is:')
   console.log(price_id);
+  console.log('product is:')
   console.log(product);
 
   if(!product?.name){
     window.location.href = '/';
   }
-  
+
   return(
     <div>
       Hello, this is the Product Page.
