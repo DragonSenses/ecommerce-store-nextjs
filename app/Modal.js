568036339e8e403a1cfab74ec95c099d087957d3
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-19 22:57:44 -0700

    Update Modal.js

--- app/Modal.js
+++ app/Modal.js
@@ -4,7 +4,14 @@ import { createPortal } from 'react-dom';
 
 export default function Modal() {
   return ( createPortal(
-    <div>Modal</div>,
+    <div className='fixed top-0 left-0 w-screen h-screen z-50'>
+      <div className="bg-transparent absolute inset-0"></div>
+      <div className="flex flex-col gap-4 p-4">
+        <div>
+          <h1>Cart</h1>
+        </div>
+      </div>
+    </div>,
     document.getElementById('portal')
   ))
 }
