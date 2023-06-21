0901879785981e265f2a565cf5f6716eabb9f653
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-20 21:58:31 -0700

    Make icon more conspicuous

--- app/Modal.js
+++ app/Modal.js
@@ -14,7 +14,7 @@ export default function Modal() {
       right-0 top-0 h-screen w-screen max-w-screen shadow-lg sm:w-96 gap-4">
         <div className='flex items-center p-6 justify-between text-xl relative'>
           <h1>Cart</h1>
-          <i onClick={closeModal} className="fa-solid fa-xmark"></i>
+          <i onClick={closeModal} className="fa-solid fa-xmark cursor-pointer hover:opacity-60"></i>
           <div className="absolute bottom-0 left-1/2-translate-x-1/2 h-[1px] w-2/3
           bg-slate-300"></div>
         </div>
