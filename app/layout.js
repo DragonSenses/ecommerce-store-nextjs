8e1c7b136bb8992af11533b869423925a349ecc5
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-23 23:04:05 -0700

    Unwrap layout with context provider

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
+"use client"
+
 import Link from 'next/link';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Header from './Header';
-import { useState, createContext } from 'react';
-import AppContext from './context/AppContext';
+import AppContextProvider from './context/AppContext';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,40 +15,37 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <AppContext.Provider>
+    <html lang="en">
 
-      <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
 
-        <head>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        </head>
+      <body className={'min-h-screen flex flex-col relative ' + inter.className}>
 
-        <body className={'min-h-screen flex flex-col relative ' + inter.className}>
+        <Header />
 
-          <Header />
+        <div className="flex-1">
+          {children}
+        </div>
 
-          <div className="flex-1">
-            {children}
+        <footer className='flex items-center flex-wrap justify-center
+        border-t border-solid border-slate-300 p-4 md:p-8'>
+          <div className="text-2xl sm:text-3xl md:text-4xl">
+            <span className="pr-4">FOOTER</span>
+            <Link href={'/'}
+            target="_blank" rel="noopener noreferrer">
+              <i className="fa-regular fa-envelope text-slate-700 hover:text-slate-500 cursor-pointer"></i>
+            </Link>
+            <Link href={'/'} 
+            target="_blank" rel="noopener noreferrer">
+              <i className="pl-4 fa-brands fa-github text-slate-700 hover:text-slate-500 cursor-pointer"></i>
+            </Link>
           </div>
+        </footer>
 
-          <footer className='flex items-center flex-wrap justify-center
-          border-t border-solid border-slate-300 p-4 md:p-8'>
-            <div className="text-2xl sm:text-3xl md:text-4xl">
-              <span className="pr-4">FOOTER</span>
-              <Link href={'/'}
-              target="_blank" rel="noopener noreferrer">
-                <i className="fa-regular fa-envelope text-slate-700 hover:text-slate-500 cursor-pointer"></i>
-              </Link>
-              <Link href={'/'} 
-              target="_blank" rel="noopener noreferrer">
-                <i className="pl-4 fa-brands fa-github text-slate-700 hover:text-slate-500 cursor-pointer"></i>
-              </Link>
-            </div>
-          </footer>
-
-          <div id="portal"></div>
-        </body>
-      </html>
-    </AppContext.Provider>
+        <div id="portal"></div>
+      </body>
+    </html>
   )
 }
