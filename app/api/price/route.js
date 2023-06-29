d588c8a066efd4c01310e30c934b781d148f38f3
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-29 12:09:39 -0700

    Implement price fetch route

--- app/api/price/route.js
+++ app/api/price/route.js
@@ -2,5 +2,35 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function GET(request) {
+  const body = await request.json();
 
+  if(body.lineItems.length === 0){
+    return new Response('Error', {
+      status: 405,
+    });
+  }
+
+  try{
+    // Initialize Stripe
+    const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+      apiVersion: '2020-08-27'
+    });
+
+    // Destructure out price_id from the request body
+    const { price_id } = body.lineItems;
+
+    // Access our product data, by retrieving the price given the id
+    const res = await stripe.prices.retrieve(
+      price_id
+    );
+
+    return NextResponse.json({ res });
+
+  } catch(err) {
+    console.log("-------- error on product page load --------");
+    console.log(err);
+    return new Response('Error', {
+      status: 500,
+    });
+  }
 }
